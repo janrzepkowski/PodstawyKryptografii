a6d31673e70aa9bc2e5bd108579b69d240bb35b8
Frank <reppepf@gmail.com>
2024-03-19 16:19:09 +0100

    Column mixing in Galois field

--- AES/src/test/java/org/example/AESTest.java
+++ AES/src/test/java/org/example/AESTest.java
@@ -6,24 +6,53 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class AESTest {
 
-    public void printTable(int[][] table) {
-        for (int i = 0; i < table.length; i++) {
-            for (int j = 0; j < table[i].length; j++) {
-                System.out.print(table[i][j] + " ");
+    public void printTable(byte[][] table) {
+        for (byte[] bytes : table) {
+            for (byte aByte : bytes) {
+                System.out.print(aByte + " ");
             }
             System.out.println();
         }
     }
 
+    byte[][] data = {{0, 4, 8, 12}, {1, 5, 9, 13}, {2, 6, 10, 14}, {3, 7, 11, 15}};
     @Test
     public void shiftRowsTest() {
         AES aes = new AES();
-        int[][] data = {{0, 4, 8, 12}, {1, 5, 9, 13}, {2, 6, 10, 14}, {3, 7, 11, 15}};
         System.out.println(data.length);
         printTable(data);
         System.out.println();
         printTable(aes.ShiftRows(data));
         assertTrue(true);
+    }
+
+    @Test
+    public void mixColumns() {
+        AES aes = new AES();
+//        printTable(data);
+//        System.out.println();
+//        aes.mixColumns(data[0]);
+//        printTable(data);
+//        char[] d = {45, 38, 49, 76};
+//        char[] d = {1, 1, 1, 1};
+        char[] d = {242, 10, 34, 92};
+//        char[] d = {198, 198, 198, 198};
+        for (char b : d) {
+            int b1 = b;
+            System.out.print(b1 + " ");
+        }
+        System.out.println();
+        char[] da = aes.mix(d);
+        for (char b : da) {
+            int b1 = b;
+            System.out.print(b1 + " ");
+        }
+        System.out.println();
+        char[] f = aes.cols(da);
+        for (char b : f) {
+            int b1 = b;
+            System.out.print(b1 + " ");
+        }
     }
 
 }
